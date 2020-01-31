7f46db6b4d6df61059f7c3fec2ba540871ca2cb8
William Thorsen <william@thorsen.dev>
2020-01-31 11:49:52 -0700

    feat: Can retrieve the name of the current branch
    Added `retrieveCurrentBranchName()`

---
+++ src/functions/shorthand/retrieveCurrentBranchName.ts
@@ -0,0 +1,10 @@
+import { getReferenceShorthand, retrieveCurrentBranchReference } from '../reference';
+import { RetrieveCurrentBranchOptions } from '../reference/retrieveCurrentBranchReference';
+
+export async function retrieveCurrentBranchName(
+  options: RetrieveCurrentBranchOptions<string> = {}
+): Promise<string | null> {
+  return retrieveCurrentBranchReference<string>(
+    { ...options, transformer: getReferenceShorthand }
+  )
+}
